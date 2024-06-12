e90b1c88159e1920def7b0b35f17e61b0e1addc0
Serge-Olivier Malouin <supcakiod@gmail.com>
2024-06-11 20:39:01 -0400

    update cd function

--- shell/commands.py
+++ shell/commands.py
@@ -9,5 +9,12 @@ def cat(file):
   with open(file, 'r') as f:
     print(f.read())
 
-def cd(path):
-  os.chdir(path)
+def cd(new_dir):
+    try:
+        os.chdir(new_dir)
+    except FileNotFoundError:
+        print(f'Directory not found: {new_dir}')
+    except NotADirectoryError:
+        print(f'Not a directory: {new_dir}')
+    except PermissionError:
+        print(f'Permission denied: {new_dir}')
