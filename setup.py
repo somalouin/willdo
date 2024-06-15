d027c5e4d084e4b4c4ca595decb53549ab0018e6
Serge-Olivier Malouin <supcakiod@gmail.com>
2024-06-15 16:30:43 -0400

    change setup

--- setup.py
+++ setup.py
@@ -5,7 +5,10 @@ setup(
     version='0.1',
     packages=find_packages(),
     author='Serge-Olivier Malouin',
-    install_requires=[
-        'blessed',
-    ]
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'm-shell = m-shell.__main__:main'
+        ]
+    }
 )
