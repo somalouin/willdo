28a13f19a7e2843db11a2861e3b1ca0605594410
Serge-Olivier Malouin <supcakiod@gmail.com>
2024-06-16 21:57:58 -0400

    format

--- m-shell/__main__.py
+++ m-shell/__main__.py
@@ -3,11 +3,14 @@
 import sys
 
 def main():
-    if len(sys.argv) > 1:
-        name = sys.argv[1]
-    else:
-        name = "World"
-    print(f"Hello, {name}!")
+  if len(sys.argv) > 1:
+      name = sys.argv[1]
+  else:
+      name = "World"
+  print(f"Hello, {name}!")
 
 if __name__ == "__main__":
-    main()
+  main()
+
+
+
