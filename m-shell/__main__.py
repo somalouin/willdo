18601ae582b80ca7749b07d797751affa16e67e1
Serge-Olivier Malouin <supcakiod@gmail.com>
2024-06-16 22:07:43 -0400

    separate files

--- m-shell/__main__.py
+++ m-shell/__main__.py
@@ -1,22 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
-
-def extract_todo_comments(file_path):
-  todo_comments = []
-  with open(file_path, 'r') as file:
-    for line_number, line in enumerate(file, start=1):
-      if 'TODO' in line:
-        todo_comments.append((line_number, line.strip()))
-  return todo_comments
-
-def display_todo_comments(todo_comments):
-  if not todo_comments:
-    print("No TODO comments found.")
-    return
-  print("TODO comments found:")
-  for line_number, comment in todo_comments:
-    print(f"Line {line_number}: {comment}")
+from todo import *
 
 def main():
   if len(sys.argv) > 1:
---
+++ m-shell/todo.py
@@ -0,0 +1,15 @@
+def extract_todo_comments(file_path):
+  todo_comments = []
+  with open(file_path, 'r') as file:
+    for line_number, line in enumerate(file, start=1):
+      if 'TODO' in line:
+        todo_comments.append((line_number, line.strip()))
+  return todo_comments
+
+def display_todo_comments(todo_comments):
+  if not todo_comments:
+    print("No TODO comments found.")
+    return
+  print("TODO comments found:")
+  for line_number, comment in todo_comments:
+    print(f"Line {line_number}: {comment}")
