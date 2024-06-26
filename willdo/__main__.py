7531e249847a834cae26072447357775b16611c6
Serge-Olivier Malouin <supcakiod@gmail.com>
2024-06-25 20:03:06 -0400

    add path error

--- willdo/__main__.py
+++ willdo/__main__.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
 import sys
+import os
 from todo import *
 
 def main():
-  if len(sys.argv) > 1:
-      name = sys.argv[1]
-      print(f"Reading file: {name}")
-  else:
-      print("> No file name provided \n")
   
-  comments = extract_todo_comments("test/test_todo.py")
+  if len(sys.argv) > 1:
+    path = sys.argv[1]
+    if not os.path.isfile(path):
+      print(f"Error: {path} is not a valid file")
+      sys.exit(1)
+  else:
+    print("> No file name provided \n")
+    sys.exit(1)
+    
+  print(f"Reading file: {path}") 
+  comments = extract_todo_comments(path)
   display_todo_comments(comments)
 
 if __name__ == "__main__":
