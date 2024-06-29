7dbf1fa92f2ef55dacf11cff15c4ea495fb18ad8
Serge-Olivier Malouin <supcakiod@gmail.com>
2024-06-28 23:15:51 -0400

    check all files in root

--- willdo/todo.py
+++ willdo/todo.py
@@ -1,17 +1,24 @@
-def extract_todo_comments(file_path):
-  todo_comments = []
-  with open(file_path, 'r') as file:
-    for line_number, line in enumerate(file, start=1):
-      if 'TODO' in line:
-        todo_comments.append((line_number, line.strip()))
+import os
+
+ROOT_DIR = os.path.abspath(os.curdir)
+
+def extract_todo_comments():
+  todo_comments = {}
+  for root, dirs, files in os.walk(ROOT_DIR):
+    for file in files:
+      file_path = os.path.join(root, file)
+      if ".git" not in file_path and ".vscode" not in file_path and ".gitignore" not in file_path:
+        with open(file_path, 'r') as file:
+          print(f"[*] Reading file: {file_path}")
+          for line_number, line in enumerate(file, start=1):
+            if 'TODO' in line:
+              if file_path not in todo_comments:
+                todo_comments[file_path] = []
+              todo_comments[file_path].append((line_number, line.strip()))
   return todo_comments
 
-def display_todo_comments(todo_comments, path):
-  if not todo_comments:
-    print("> No TODO in file")
-    return
-  print(f"> TODO list for {path}")
-  for line_number, comment in todo_comments:
-    print(f"--> {comment.split('-')[1].strip()} (line {line_number})")
-
-  print("\n")
+def display_todo_comments(todo_comments):
+  for file_path, comments in todo_comments.items():
+    print(f"File: {file_path}")
+    for line_number, comment in comments:
+      print(f"Line {line_number}: {comment}")
