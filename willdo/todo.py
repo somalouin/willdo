ade416731acdfca23b53b8ecb0e8ed647f206f8a
Serge-Olivier Malouin <supcakiod@gmail.com>
2024-06-28 23:26:56 -0400

    check for TODO comments in specific

--- willdo/todo.py
+++ willdo/todo.py
@@ -9,9 +9,9 @@ def extract_todo_comments():
       file_path = os.path.join(root, file)
       if ".git" not in file_path and ".vscode" not in file_path and ".gitignore" not in file_path:
         with open(file_path, 'r') as file:
-          print(f"[*] Reading file: {file_path}")
+          # print(f"[*] Reading file: {file_path}")
           for line_number, line in enumerate(file, start=1):
-            if 'TODO' in line:
+            if "# TODO" in line:
               if file_path not in todo_comments:
                 todo_comments[file_path] = []
               todo_comments[file_path].append((line_number, line.strip()))
@@ -21,4 +21,5 @@ def display_todo_comments(todo_comments):
   for file_path, comments in todo_comments.items():
     print(f"File: {file_path}")
     for line_number, comment in comments:
-      print(f"Line {line_number}: {comment}")
+      #print(f"Line {line_number}: {comment}")
+      print(f"{comment.split('TODO')[1].strip()} (line {line_number})")
