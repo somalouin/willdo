8d65ab7a44bf0a8b7cdffae50ec79fe4d49616c0
Serge-Olivier Malouin <supcakiod@gmail.com>
2024-06-29 10:30:51 -0400

    fix display formatting

--- test/test_todo.py
+++ test/test_todo.py
@@ -1,14 +1,14 @@
 from willdo.todo import extract_todo_comments
 
 def test_extract_todo_comments():
-  # TODO - aaa
+  # TODO: aaa
   # test comment
-  # TODO - bbb
-  # TODO - ccc
+  # TODO: bbb
+  # TODO: ccc
   # test comment
   # test comment
   # test comment
-  # TODO - ddd
-  # TODO - eee
+  # TODO: ddd
+  # TODO: eee
 
   assert len(extract_todo_comments("test/test_todo.py")) == 4
--- willdo/todo.py
+++ willdo/todo.py
@@ -7,11 +7,10 @@ def extract_todo_comments():
   for root, dirs, files in os.walk(ROOT_DIR):
     for file in files:
       file_path = os.path.join(root, file)
-      if ".git" not in file_path and ".vscode" not in file_path and ".gitignore" not in file_path:
+      if ".git" not in file_path and ".vscode" not in file_path and ".gitignore" not in file_path and "README.md" not in file_path:
         with open(file_path, 'r') as file:
-          # print(f"[*] Reading file: {file_path}")
           for line_number, line in enumerate(file, start=1):
-            if "# TODO" in line:
+            if "# TODO:" in line:
               if file_path not in todo_comments:
                 todo_comments[file_path] = []
               todo_comments[file_path].append((line_number, line.strip()))
@@ -21,5 +20,4 @@ def display_todo_comments(todo_comments):
   for file_path, comments in todo_comments.items():
     print(f"File: {file_path}")
     for line_number, comment in comments:
-      #print(f"Line {line_number}: {comment}")
-      print(f"{comment.split('TODO')[1].strip()} (line {line_number})")
+      print(f"-> {comment.split('TODO:')[1].strip()} (line {line_number})")
