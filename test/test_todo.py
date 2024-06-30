fc02931699ca2177b395906b46cfb96e16471dce
Serge-Olivier Malouin <supcakiod@gmail.com>
2024-06-30 17:41:48 -0400

    update tests

--- test/test_todo.py
+++ test/test_todo.py
@@ -1,14 +1,16 @@
-from willdo.todo import extract_todo_comments
-
+from willdo.todo import *
 def test_extract_todo_comments():
   # TODO: aaa
-  # test comment
   # TODO: bbb
   # TODO: ccc
-  # test comment
-  # test comment
-  # test comment
   # TODO: ddd
   # TODO: eee
+  comments = search_file("test/test_todo.py")
+  assert len(comments) == 1
 
-  assert len(extract_todo_comments("test/test_todo.py")) == 4
+def test_display_todo_comments(capsys):
+  comments = {"test": [(1, 'TODO: aaa'), (2, 'TODO: bbb')]}
+  display_todo_comments(comments)
+  captured = capsys.readouterr()
+  all_outputs = captured.out.split('\n')
+  assert len(all_outputs) == 4
