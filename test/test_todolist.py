0381166292202d9338b8ab9cbc6cd03467e6627a
Serge-Olivier Malouin <supcakiod@gmail.com>
2024-07-03 21:28:46 -0400

    remove todo test

--- test/test_todolist.py
+++ test/test_todolist.py
@@ -1,16 +1,5 @@
-from willdo.todo import *
-def test_extract_todo_comments():
-  # TODO: aaa
-  # TODO: bbb
-  # TODO: ccc
-  # TODO: ddd
-  # TODO: eee
-  comments = search_file("test/test_todo.py")
-  assert len(comments) == 1
-
-def test_display_todo_comments(capsys):
-  comments = {"test": [(1, 'TODO: aaa'), (2, 'TODO: bbb')]}
-  display_todo_comments(comments)
-  captured = capsys.readouterr()
-  all_outputs = captured.out.split('\n')
-  assert len(all_outputs) == 4
+# TODO: aaa
+# TODO: bbb
+# TODO: ccc
+# TODO: ddd
+# TODO: eee
