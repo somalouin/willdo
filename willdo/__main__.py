6e191a127792029e0570f71e6cc45ea80184e4ae
Serge-Olivier Malouin <supcakiod@gmail.com>
2024-07-05 17:51:38 -0400

    add file path in args

--- willdo/__main__.py
+++ willdo/__main__.py
@@ -6,7 +6,10 @@ from features import list, export
 def main():
   if len(sys.argv) > 1:
     if sys.argv[1] == "list":
-      list()
+      if len(sys.argv) > 2 and sys.argv[2].endswith('.py'):
+        list(sys.argv[2])
+      else:
+        list()
     elif sys.argv[1] == "export":
       export()
   else:
--- willdo/features.py
+++ willdo/features.py
@@ -1,8 +1,14 @@
 from willdo.todo import *
 
-def list():
-  display_todos(process_files())
+def list(path=None):
+  if path and os.path.isfile(path):
+    display_todos(process_files(path))
+  else:
+    display_todos(process_files())
   
 
-def export():
-  export_to_markdown(process_files())
+def export(path=None):
+  if path and os.path.isfile(path):
+    export_to_markdown(process_files(path))
+  else:
+    export_to_markdown(process_files())
