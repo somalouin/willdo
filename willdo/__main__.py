7fbe8d85775cd8cbb4bd24c80770346aa09d741d
Serge-Olivier Malouin <supcakiod@gmail.com>
2024-07-29 21:47:42 -0400

    add banner at launch

--- willdo/__main__.py
+++ willdo/__main__.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
 import sys
+import pyfiglet
 from features import list, export
 from utils.input import handle_params
 
 def main():
   if len(sys.argv) > 1:
+    print(pyfiglet.figlet_format("willdo"))
     if sys.argv[1] == "list": handle_params(sys.argv, list)
     elif sys.argv[1] == "export": handle_params(sys.argv, export)
   else:
